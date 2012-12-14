f1095be570199152ab15e331381fbdbff84a084f
Leif Högberg <leihog@gmail.com>
2012-12-14 15:49:19 +0100

    fix typo

--- lib/world/church_inner.lua
+++ lib/world/church_inner.lua
@@ -1,8 +1,8 @@
 inherit = "/std/room"
 
-short_desc = "Village church: inner room"
+short_desc = "Village church: inner sanctum"
 long_desc = "You are in the inner sanctum of the church.\n" ..
-	"This is where the clergy come to contemplate lifes mysterious in silence."
+	"This is where the clergy come to contemplate lifes mysteries in silence."
 exits = {
 	east = "/world/church",
 }
